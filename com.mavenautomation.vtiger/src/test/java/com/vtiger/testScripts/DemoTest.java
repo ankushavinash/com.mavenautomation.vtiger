cf1f37c6a7eacae66dab90ccd468cef35a05ca0f
Ankush <Ankush@Ankush>
2020-11-17 08:03:27 +0530

    my 2nd commit of vtiger

---
+++ com.mavenautomation.vtiger/src/test/java/com/vtiger/testScripts/DemoTest.java
@@ -0,0 +1,13 @@
+package com.vtiger.testScripts;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void checkGit()
+	{
+		System.out.println("Checking git upload commit 2");
+	}
+
+}
